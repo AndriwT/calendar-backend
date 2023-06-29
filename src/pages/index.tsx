9dd0b7e955759c1436698c4002e860461ea31398
Andriw Tapanes <andriwtapanes@gmail.com>
2023-06-28 22:24:19 -0400

    working on get function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,33 @@ import {
   limit,
   getDocs,
 } from "@firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const calendarDayCollection = collection(firestore, "dayData");
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [dayData, setDayData] = useState<QueryDocumentSnapshot<DocumentData>[]>(
-    []
-  );
   const [loading, setLoading] = useState<boolean>(true);
+  const [dayData, setDayData] = useState<QueryDocumentSnapshot<DocumentData>>();
+
+  const getDayData = async () => {
+    const dayDataQuery = query(daysCollection, where("id", "==", true));
+
+    const querySnapshot = await getDocs(dayDataQuery);
+
+    const result: QueryDocumentSnapshot<DocumentData> = querySnapshot;
+
+    setDayData(result);
+  };
+
+  useEffect(() => {
+    getDayData();
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <main
@@ -27,6 +43,15 @@ export default function Home() {
     >
       <div>
         <p>Hello world!</p>
+      </div>
+      <div className="">
+        {loading ? (
+          <div className="">
+            <h2>Loading</h2>
+          </div>
+        ) : (
+          <p>dayData</p>
+        )}
       </div>
     </main>
   );
